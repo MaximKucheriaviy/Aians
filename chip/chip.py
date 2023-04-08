7320a5dd81fab6fbeee5b58e66b1ff143b2a4183
Maxim Kucheriaviy <maxim-k@i.ua>
2023-04-08 10:09:54 +0300

    ok go

--- chip/chip.py
+++ chip/chip.py
@@ -1,7 +1,10 @@
 import pygame
 
+
 class Chip:
     def __init__(self, app, number = 0, ):
+        if not pygame.font.get_init():
+            pygame.font.init()
         self.number = number
         self.settings = app.settings
         self.screen = app.screen
@@ -11,6 +14,19 @@ class Chip:
         self.rect.width = rectSize
         self.rect.height = rectSize
         self.image = pygame.transform.scale(self.image, (rectSize, rectSize))
+    def isHover(self):
+        mousePoss = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mousePoss[0], mousePoss[1]):
+            return True
+        return 
+    def isPressed(self):
+        if not self.isHover():
+            return False
+        if pygame.mouse.get_pressed()[0]:
+            return True
+        return False
     def display(self, rect):
+        if self.isPressed():
+            print(self.number)
         self.rect.center = rect.center
         self.screen.blit(self.image, self.rect)
--- gameField/gameField.py
+++ gameField/gameField.py
@@ -6,21 +6,25 @@ class GameField:
         self.screen = app.screen
         self.settings = app.settings
         self.fields = []
-        self.chips = []
         for i in range(0, 4):
             self.fields.append([])
-            self.chips.append([])
             for j in range(0, 4):
                 borderSize = self.settings.gameFieldSize / 4
                 left = self.settings.gameFieldLeft + (borderSize * j)
                 top = self.settings.gameFieldTop + (borderSize * i)
-                self.fields[i].append(pygame.rect.Rect(left, top, borderSize, borderSize))
-                self.chips[i].append(Chip(app))
+                chip = None
+                if i != 3 or j != 3:
+                    chip = Chip(app, j + 1 + i * 4)
+                obj = {'field': pygame.rect.Rect(left, top, borderSize, borderSize), 'chip': chip}
+                self.fields[i].append(obj)
 
     def display(self):
         for item in self.fields:
             for rect in item:
-                pygame.draw.rect(self.screen, pygame.Color(0, 255, 0), rect)
-        for i in range(0, 4):
-            for j in range(0, 4):
-                self.chips[i][j].display(self.fields[i][j])
+                pygame.draw.rect(self.screen, pygame.Color(0, 255, 0), rect['field'])
+        for item in self.fields:
+            for field in item:
+                if(field['chip'] is None):
+                    continue
+                field['chip'].display(field['field'])
+                
